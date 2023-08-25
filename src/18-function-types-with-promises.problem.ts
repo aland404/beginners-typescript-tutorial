1c437521f841be0b2279f2f99905f2799eabd8e5
Alan Duchêne <alan.duchene@zenika.com>
2023-08-25 09:09:46 +0200

    feat: solve exercice 18

--- src/18-function-types-with-promises.problem.ts
+++ src/18-function-types-with-promises.problem.ts
@@ -7,8 +7,8 @@ interface User {
 }
 
 const createThenGetUser = async (
-  createUser: unknown,
-  getUser: unknown,
+  createUser: () => Promise<User['id']>,
+  getUser: (userId: User['id']) => Promise<User>,
 ): Promise<User> => {
   const userId: string = await createUser();
 
